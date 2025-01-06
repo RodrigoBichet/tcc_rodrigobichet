77477c90eb3ccd94fcc298af4512232baefc4437
Rodrigo Bichet <rodrigobichet.pl010@academico.ifsul.edu.br>
2025-01-05 21:53:40 -0300

    update

--- db.js
+++ db.js
@@ -1,12 +1,24 @@
+// const { Pool } = require("pg");
+
+// // Configurações do banco de dados
+// const pool = new Pool({
+//     user: "postgres", // Substitua pelo seu usuário do PostgreSQL
+//     host: "localhost", // Endereço do servidor
+//     database: "tcc_rodrigobichet", // Substitua pelo nome do seu banco
+//     password: "Rootrb2025..", // Substitua pela senha do seu banco
+//     port: 5432, // Porta padrão do PostgreSQL
+// });
+
+// module.exports = pool;
+
 const { Pool } = require("pg");
 
-// Configurações do banco de dados
+// Configurações do pool de conexão
 const pool = new Pool({
-    user: "postgres", // Substitua pelo seu usuário do PostgreSQL
-    host: "localhost", // Endereço do servidor
-    database: "tcc_rodrigobichet", // Substitua pelo nome do seu banco
-    password: "Rootrb2025..", // Substitua pela senha do seu banco
-    port: 5432, // Porta padrão do PostgreSQL
+    connectionString: process.env.DATABASE_URL, // Render fornecerá essa variável
+    ssl: {
+        rejectUnauthorized: false, // Necessário para conexões seguras em produção
+    },
 });
 
 module.exports = pool;
