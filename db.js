e7a90daf5397c9aeafcf3527da3d95acf478f21d
Rodrigo Bichet <rodrigobichet.pl010@academico.ifsul.edu.br>
2025-01-05 22:12:04 -0300

    render update

--- db.js
+++ db.js
@@ -11,14 +11,26 @@
 
 // module.exports = pool;
 
+// const { Pool } = require("pg");
+
+// // Configurações do pool de conexão
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL, // Render fornecerá essa variável
+//     ssl: {
+//         rejectUnauthorized: false, // Necessário para conexões seguras em produção
+//     },
+// });
+
+// module.exports = pool;
+
 const { Pool } = require("pg");
 
-// Configurações do pool de conexão
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // Render fornecerá essa variável
-    ssl: {
-        rejectUnauthorized: false, // Necessário para conexões seguras em produção
-    },
+    user: "postgres", // Seu usuário do banco
+    host: "db.render.com", // Substitua pelo endereço correto do seu banco na Render
+    database: "tcc_rodrigobichet", // Nome do seu banco de dados
+    password: "Rootrb2025..", // A senha correta do banco
+    port: 5432, // Porta padrão do PostgreSQL
 });
 
 module.exports = pool;
